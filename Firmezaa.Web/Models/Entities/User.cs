dd338fb6cd9fdd884442bbab0cb9900bc8241435
Dylan Marin Campo <dylanriwi.dev@gmail.com>
2025-11-14 16:58:04 -0500

    refactor: remove Client and Person classes and update User model

--- Firmezaa.Web/Models/Entities/User.cs
+++ Firmezaa.Web/Models/Entities/User.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 
+
 namespace Firmezaa.Web.Models.Entities;
 
 public class User : IdentityUser
@@ -11,10 +12,15 @@ public class User : IdentityUser
     [MaxLength(50)]
     public string? DocumentNumber { get; set; }
 
+    [Range(0, 150, ErrorMessage = "La edad debe estar entre 0 y 150 años.")]
+    public int Age { get; set; }
+
     [MaxLength(200)]
     public string? Address { get; set; }
     
     [DataType(DataType.Date)]
     public DateTime RegisterDate { get; set; } = DateTime.UtcNow;
+
+    public ICollection<Sale> Sales { get; set; } = new List<Sale>();
     
 }
